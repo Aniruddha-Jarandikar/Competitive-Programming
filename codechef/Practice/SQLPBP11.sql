c381211cdb9ebf561787343eef0cfba0432e332a
Aniruddha Jarandikar <aniruddhajarandikar@gmail.com>
2026-09-02 20:28:51 +0530

    Solved: SQLPBP11 (codechef)

---
+++ codechef/Practice/SQLPBP11.sql
@@ -0,0 +1,9 @@
+// Problem: SQLPBP11
+// Platform: codechef
+// Language: SQL​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLBP01/problems/SQLPBP11
+// Solved on: 2026-09-02T14:58:56.561Z
+
+SELECT *
+FROM Products;
