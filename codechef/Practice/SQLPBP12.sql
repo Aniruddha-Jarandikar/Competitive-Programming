14bdaa81e8898b9ca8ca7d9518b650398fad3a47
Aniruddha Jarandikar <aniruddhajarandikar@gmail.com>
2026-09-02 20:29:03 +0530

    Solved: SQLPBP12 (codechef)

---
+++ codechef/Practice/SQLPBP12.sql
@@ -0,0 +1,10 @@
+// Problem: SQLPBP12
+// Platform: codechef
+// Language: SQL​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLBP01/problems/SQLPBP12
+// Solved on: 2026-09-02T14:59:08.417Z
+
+SELECT product_name, category
+FROM Products
+WHERE price > 100.00;
