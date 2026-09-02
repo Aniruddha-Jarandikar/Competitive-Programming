f6d4fef32476bdec6da91f65fa0b8d8ec0a41e12
Aniruddha Jarandikar <aniruddhajarandikar@gmail.com>
2026-09-02 20:29:18 +0530

    Solved: SQLPBP14 (codechef)

--- codechef/Practice/SQLPBP14.sql
+++ codechef/Practice/SQLPBP14.sql
@@ -3,7 +3,7 @@
 // Language: SQL​
 // Verdict: Accepted
 // URL: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLBP01/problems/SQLPBP14
-// Solved on: 2026-09-02T14:59:16.815Z
+// Solved on: 2026-09-02T14:59:22.823Z
 
 SELECT AVG(salary) AS avg_salary
 FROM Works;
