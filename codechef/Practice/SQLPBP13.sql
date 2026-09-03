1471e1905f534e7fd59bad3d9376b1f533b98b54
Aniruddha Jarandikar <aniruddhajarandikar@gmail.com>
2026-09-03 18:24:20 +0530

    Solved: SQLPBP13 (codechef)

---
+++ codechef/Practice/SQLPBP13.sql
@@ -0,0 +1,9 @@
+// Problem: SQLPBP13
+// Platform: codechef
+// Language: SQL​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLBP01/problems/SQLPBP13
+// Solved on: 2026-09-03T12:54:21.095Z
+
+SELECT DISTINCT company_name
+FROM Works;
