f05ca31e0d876c30bd36f4243980a9543d6b1fd2
Aniruddha Jarandikar <aniruddhajarandikar@gmail.com>
2026-09-03 18:27:30 +0530

    Solved: SQLPBP07 (codechef)

---
+++ codechef/Practice/SQLPBP07.sql
@@ -0,0 +1,10 @@
+// Problem: SQLPBP07
+// Platform: codechef
+// Language: SQL​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLBP01/problems/SQLPBP07
+// Solved on: 2026-09-03T12:57:30.636Z
+
+SELECT book_id, title, author, published_year
+FROM Library
+WHERE rating IS NULL;
