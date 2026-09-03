8b7b16664e7ec9876c07b5606cd3aa442ca9c633
Aniruddha Jarandikar <aniruddhajarandikar@gmail.com>
2026-09-03 18:29:35 +0530

    Solved: SQLPBP06 (codechef)

---
+++ codechef/Practice/SQLPBP06.sql
@@ -0,0 +1,11 @@
+// Problem: SQLPBP06
+// Platform: codechef
+// Language: SQL​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLBP01/problems/SQLPBP06
+// Solved on: 2026-09-03T12:59:35.483Z
+
+SELECT DISTINCT author_id, author_name, publication_name
+FROM Views
+WHERE view_count = 0
+ORDER BY author_id ASC;
