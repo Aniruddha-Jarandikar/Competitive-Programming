f26d1f8e15dc512dded00507c8588d8bdf6c1e22
Aniruddha Jarandikar <aniruddhajarandikar@gmail.com>
2026-09-03 18:30:07 +0530

    Solved: SQLPBP04 (codechef)

---
+++ codechef/Practice/SQLPBP04.c
@@ -0,0 +1,18 @@
+// Problem: SQLPBP04
+// Platform: codechef
+// Language: ┌─────────────┬───────┐
+│ player_name │ score │
+├─────────────┼───────┤
+│ David       │ 1600  │
+│ Bob         │ 1500  │
+│ Charlie     │ 1300  │
+└─────────────┴───────┘
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLBP01/problems/SQLPBP04
+// Solved on: 2026-09-03T13:00:07.673Z
+
+SELECT DISTINCT p.player_name, p.score
+FROM Players p
+JOIN Matches m ON p.player_name = m.winner
+ORDER BY p.score DESC
+LIMIT 3;
