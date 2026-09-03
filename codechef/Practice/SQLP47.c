9b34bce249030555374d2ef94469fb68b16955c7
Aniruddha Jarandikar <aniruddhajarandikar@gmail.com>
2026-09-03 18:34:04 +0530

    Solved: Choose the Output of following query ? (codechef)

---
+++ codechef/Practice/SQLP47.c
@@ -0,0 +1,8 @@
+// Problem: Choose the Output of following query ?
+// Platform: codechef
+// Language: select MIN(college) from Users;
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLPRAC05/problems/SQLP47
+// Solved on: 2026-09-03T13:04:04.913Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
