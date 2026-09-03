01695740ba5542ad461b72afe8ed80989a1b424f
Aniruddha Jarandikar <aniruddhajarandikar@gmail.com>
2026-09-03 18:31:31 +0530

    Solved: Spot the Error in following query (codechef)

---
+++ codechef/Practice/SQLP42.c
@@ -0,0 +1,8 @@
+// Problem: Spot the Error in following query
+// Platform: codechef
+// Language: Select count(),user_name from Users;
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLPRAC05/problems/SQLP42
+// Solved on: 2026-09-03T13:01:31.536Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
