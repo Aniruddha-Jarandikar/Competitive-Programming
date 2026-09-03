02ab72f189f6a9b623671adbabf20b3d406605ae
Aniruddha Jarandikar <aniruddhajarandikar@gmail.com>
2026-09-03 18:26:39 +0530

    Solved: SQLPBP01 (codechef)

--- codechef/Practice/SQLPBP01.sql
+++ codechef/Practice/SQLPBP01.sql
@@ -3,7 +3,7 @@
 // Language: SQL​
 // Verdict: Accepted
 // URL: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLBP01/problems/SQLPBP01
-// Solved on: 2026-09-03T12:56:35.507Z
+// Solved on: 2026-09-03T12:56:40.068Z
 
 SELECT Movie_name
 FROM Cinema
