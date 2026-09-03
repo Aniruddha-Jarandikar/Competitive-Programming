ee7542de57bce35abd3e9d40afe1de0d0a46aabb
Aniruddha Jarandikar <aniruddhajarandikar@gmail.com>
2026-09-03 18:28:50 +0530

    Solved: SQLPBP09 (codechef)

--- codechef/Practice/SQLPBP09.sql
+++ codechef/Practice/SQLPBP09.sql
@@ -3,7 +3,7 @@
 // Language: SQL​
 // Verdict: Accepted
 // URL: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLBP01/problems/SQLPBP09
-// Solved on: 2026-09-03T12:58:46.283Z
+// Solved on: 2026-09-03T12:58:50.866Z
 
 SELECT department, COUNT(*) AS total_employees
 FROM Employees
