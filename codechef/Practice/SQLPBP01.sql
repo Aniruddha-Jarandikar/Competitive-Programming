719e6248267bd6ea8c98066aa8823268989ad02c
Aniruddha Jarandikar <aniruddhajarandikar@gmail.com>
2026-09-03 18:26:35 +0530

    Solved: SQLPBP01 (codechef)

---
+++ codechef/Practice/SQLPBP01.sql
@@ -0,0 +1,10 @@
+// Problem: SQLPBP01
+// Platform: codechef
+// Language: SQL​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLBP01/problems/SQLPBP01
+// Solved on: 2026-09-03T12:56:35.507Z
+
+SELECT Movie_name
+FROM Cinema
+WHERE Rating > 7 AND Rating < 9;
