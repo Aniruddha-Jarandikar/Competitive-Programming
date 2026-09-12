805f0ace44247bfa61bea9928f3c0789fd5489db
Aniruddha Jarandikar <aniruddhajarandikar@gmail.com>
2026-09-12 19:32:21 +0530

    Solved: LPYAS10 (codechef)

---
+++ codechef/Practice/LPYAS10.py
@@ -0,0 +1,8 @@
+// Problem: LPYAS10
+// Platform: codechef
+// Language: Python3​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/python/LPPYAS01/problems/LPYAS10
+// Solved on: 2026-09-12T14:02:24.606Z
+
+print("Learn Coding on CodeChef" )
