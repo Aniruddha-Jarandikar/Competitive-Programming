6c33983df67617ca19f3b0a0c06f38f9d1029867
Aniruddha Jarandikar <aniruddhajarandikar@gmail.com>
2026-09-12 19:34:11 +0530

    Solved: LPYAS10B (codechef)

---
+++ codechef/Practice/LPYAS10B.py
@@ -0,0 +1,9 @@
+// Problem: LPYAS10B
+// Platform: codechef
+// Language: Python3​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/python/LPPYAS01/problems/LPYAS10B
+// Solved on: 2026-09-12T14:04:13.875Z
+
+for i in range(1,6):
+    print("*" * i)
