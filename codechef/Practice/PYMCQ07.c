7cd7d6076383fa6f8974cc20c5ab0acede1eaf96
Aniruddha Jarandikar <aniruddhajarandikar@gmail.com>
2026-09-12 19:50:49 +0530

    Solved: PYMCQ07 (codechef)

---
+++ codechef/Practice/PYMCQ07.c
@@ -0,0 +1,9 @@
+// Problem: PYMCQ07
+// Platform: codechef
+// Language: a, b, c = 1, 2, 3
+print(b)
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/python-interview-questions/PYPREP02/problems/PYMCQ07
+// Solved on: 2026-09-12T14:20:51.864Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
