19466b16421a1930ed7e7d0d8e3f5447ce1d53e3
Aniruddha Jarandikar <aniruddhajarandikar@gmail.com>
2026-09-12 19:42:52 +0530

    Solved: PREP55 (codechef)

---
+++ codechef/Practice/PREP55.py
@@ -0,0 +1,21 @@
+// Problem: PREP55
+// Platform: codechef
+// Language: Python3​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/linked-lists-new/LINKEDP04/problems/PREP55
+// Solved on: 2026-09-12T14:12:55.425Z
+
+class Solution:
+    def removeDuplicates(self, head):
+        current = head
+        
+        # Traverse the list until the end
+        while current and current.next:
+            # If current node and next node have the same data, skip the next node
+            if current.data == current.next.data:
+                current.next = current.next.next
+            else:
+                # Otherwise, move to the next distinct node
+                current = current.next
+                
+        return head
