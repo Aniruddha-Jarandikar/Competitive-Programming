1cb4698b9f0dc66c6fe68a4a17a99d4bbf177a25
Aniruddha Jarandikar <aniruddhajarandikar@gmail.com>
2026-09-12 19:31:50 +0530

    Solved: LPYAS09 (codechef)

---
+++ codechef/Practice/LPYAS09.py
@@ -0,0 +1,8 @@
+// Problem: LPYAS09
+// Platform: codechef
+// Language: Python3​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/python/LPPYAS01/problems/LPYAS09
+// Solved on: 2026-09-12T14:01:52.274Z
+
+print(12*9)
